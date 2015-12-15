3fe19ba1bf3af1c6b008d7b6fc4ece1d1ea164a9
aadesh <aadesh.mahajan@gmail.com>
2015-12-15 14:27:44 +0530

    15122015 update domain again aadesh

--- LearningOnline/src/main/java/fss/webportal/lo/domain/CategoryClass.java
+++ LearningOnline/src/main/java/fss/webportal/lo/domain/CategoryClass.java
@@ -5,11 +5,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 @Entity
 @Table(name="os_category_class")
 public class CategoryClass 
-{
+{	
+	public CategoryClass(){
+	}
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="PK_category_class_id")
@@ -19,15 +23,76 @@ public class CategoryClass
 	@Column(name="cc_count_answer")
 	private int countAnswer;
 	
+	@ManyToOne
+	@JoinColumn(name="FK_course_id")
+	private CourseCategory courseCategory;
+	@ManyToOne
+	@JoinColumn(name="FK_subject_id")
+	private SubjectCategory subjectCategory;
+	@ManyToOne
+	@JoinColumn(name="FK_chapter_id")
+	private ChapterCategory chapterCategory;
+	@ManyToOne
+	@JoinColumn(name="FK_topic_id")
+	private TopicCategory topicCategory;
 	
 	
-	
+	public int getCategoryClassId() {
+		return categoryClassId;
+	}
+
+	public void setCategoryClassId(int categoryClassId) {
+		this.categoryClassId = categoryClassId;
+	}
+
+	public int getCountQuestion() {
+		return countQuestion;
+	}
+
+	public void setCountQuestion(int countQuestion) {
+		this.countQuestion = countQuestion;
+	}
+
+	public int getCountAnswer() {
+		return countAnswer;
+	}
+
+	public void setCountAnswer(int countAnswer) {
+		this.countAnswer = countAnswer;
+	}
+
+	public CourseCategory getCourseCategory() {
+		return courseCategory;
+	}
+
+	public void setCourseCategory(CourseCategory courseCategory) {
+		this.courseCategory = courseCategory;
+	}
+
+	public SubjectCategory getSubjectCategory() {
+		return subjectCategory;
+	}
+
+	public void setSubjectCategory(SubjectCategory subjectCategory) {
+		this.subjectCategory = subjectCategory;
+	}
+
+	public ChapterCategory getChapterCategory() {
+		return chapterCategory;
+	}
+
+	public void setChapterCategory(ChapterCategory chapterCategory) {
+		this.chapterCategory = chapterCategory;
+	}
+
+	public TopicCategory getTopicCategory() {
+		return topicCategory;
+	}
+
+	public void setTopicCategory(TopicCategory topicCategory) {
+		this.topicCategory = topicCategory;
+	}
 
 	
-	public CategoryClass()
-	{
-		
-		
-	}
 
 }
--- LearningOnline/src/main/java/fss/webportal/lo/domain/ChapterCategory.java
+++ LearningOnline/src/main/java/fss/webportal/lo/domain/ChapterCategory.java
@@ -2,26 +2,45 @@ package fss.webportal.lo.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name="os_chapter_cat")
 public class ChapterCategory implements Serializable
 {
+    public ChapterCategory(){
+	}
+    
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="PK_chc_chpter_id")
 	private int chapterId;
 	@Column(name="chc_title")
+	@NotEmpty
 	private String chapterTitle;
 	@Column(name="chc_modify_date")
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date chapterModifyDate;
 	@Column(name="chc_status")
 	private int status;
@@ -30,10 +49,19 @@ public class ChapterCategory implements Serializable
 	@Column(name="chc_remark")
 	private String remark;
 	
-	public ChapterCategory(){
-		
-	}
-
+	@ManyToOne
+	@JoinTable(name="os_subject_chapter",  
+	joinColumns={@JoinColumn(name="FK_subject_id",referencedColumnName="PK_suc_subject_id")}, 
+    inverseJoinColumns={@JoinColumn(name="FK_chapter_id",referencedColumnName="PK_chc_chpter_id")}) 
+	private SubjectCategory subjectCategory;
+	@JsonIgnore
+	@OneToMany(fetch = FetchType.LAZY,mappedBy="chapterCategory", cascade=CascadeType.ALL)
+	private Set<TopicCategory> topicCategory;
+	
+	@JsonIgnore
+    @OneToMany(fetch = FetchType.LAZY,mappedBy="chapterCategory", cascade=CascadeType.ALL) 
+    private Set<CategoryClass> categoryClasses;
+	
 	public int getChapterId() {
 		return chapterId;
 	}
@@ -82,10 +110,29 @@ public class ChapterCategory implements Serializable
 		this.remark = remark;
 	}
 	
-	
-	
-	
-	
+	public SubjectCategory getSubjectCategory() {
+		return subjectCategory;
+	}
+
+	public void setSubjectCategory(SubjectCategory subjectCategory) {
+		this.subjectCategory = subjectCategory;
+	}
+
+	public Set<TopicCategory> getTopicCategory() {
+		return topicCategory;
+	}
+
+	public void setTopicCategory(Set<TopicCategory> topicCategory) {
+		this.topicCategory = topicCategory;
+	}
+
+	public Set<CategoryClass> getCategoryClasses() {
+		return categoryClasses;
+	}
+
+	public void setCategoryClasses(Set<CategoryClass> categoryClasses) {
+		this.categoryClasses = categoryClasses;
+	}
 	
 	
 
